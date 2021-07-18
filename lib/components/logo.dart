c66611cf044a3235f97984894958d46a374d5f64
Abubaker Tagelsir <abubaker.tagelsir@gmail.com>
2021-07-18 10:45:48 +0400

    draft design

---
+++ lib/components/logo.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class OurLogo extends StatelessWidget {
+  const OurLogo({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text("Logo"),
+    );
+  }
+}
